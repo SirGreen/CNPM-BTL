6172303bd9c2a06c72db89e828a92abf5ef81e18
Huỳnh Minh Khoa <61875709+SirGreen@users.noreply.github.com>
2024-12-07 23:00:44 +0700

    Updated upload files

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -28,5 +28,10 @@ namespace InventoryManagementDemo.Controllers
         {
             return View();
         }
+
+        public IActionResult PageConfig()
+        {
+            return View();
+        }
     }
 }
